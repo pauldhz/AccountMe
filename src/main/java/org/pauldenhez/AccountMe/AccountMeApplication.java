91612c53b5e25dfa2f1ff3ff73a8b20089ac5c23
pauldhz <paul.denhez.ds@gmail.com>
2025-02-20 19:28:26 +0100

    add minimal spring app managed by gradle

---
+++ src/main/java/org/pauldenhez/AccountMe/AccountMeApplication.java
@@ -0,0 +1,13 @@
+package org.pauldenhez.AccountMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pauldenhez/AccountMe/AccountMeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pauldenhez.AccountMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
