26dd176122f444277579b84697341c355de4770c
Maxime Rose <maxime.rose@hotmail.fr>
2025-04-16 10:07:55 +0200

    Connected to front

--- src/main/java/org/pauldenhez/AccountMe/controller/TransactionController.java
+++ src/main/java/org/pauldenhez/AccountMe/controller/TransactionController.java
@@ -3,53 +3,54 @@ package org.pauldenhez.AccountMe.controller;
 import org.pauldenhez.AccountMe.model.Transaction;
 import org.pauldenhez.AccountMe.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.util.Streamable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("")
 public class TransactionController {
 
     @Autowired
     private TransactionRepository transactionRepository;
 
-    @GetMapping(path = "test")
-    public ResponseEntity<List<Transaction>> testlala() {
+    @GetMapping(path = "/transactions")
+    public ResponseEntity<List<Transaction>> listAll() {
         Iterable<Transaction> iterable = transactionRepository.findAll();
+        List<Transaction> transactions = new ArrayList<>();
         for(Transaction transaction : iterable) {
             System.out.println(transaction);
+            transactions.add(transaction);
         }
 
-        return ResponseEntity.ok(Streamable.of(iterable).toList());
+        return ResponseEntity.ok(transactions);
     }
 
-    @GetMapping(path = "/add")
-    public ResponseEntity<Transaction> add() {
-        Transaction savedTransaction = new Transaction();
-        savedTransaction.setTransactionDate(new Date());
-        savedTransaction.setLabel("ICI un label de CB");
-        savedTransaction.setDebit(12.99);
-        transactionRepository.save(savedTransaction);
+    @PutMapping(path = "/transactions")
+    public ResponseEntity<Transaction> createTransaction(@RequestBody Transaction transaction) {
+        Transaction savedTransaction = transactionRepository.save(transaction);
         return ResponseEntity.ok(savedTransaction);
     }
 
-    @GetMapping(path = "/{id}")
+    @GetMapping(path = "/transactions/{id}")
     public ResponseEntity<Transaction> getById(@PathVariable String id) {
-        Optional<Transaction> tran = transactionRepository.findById(id);
-        if(tran.isPresent()) {
-            Transaction savedTransaction = tran.get();
-            savedTransaction.setDebit(tran.get().getDebit()+1);
-            transactionRepository.save(savedTransaction);
-        }
-        return tran.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        Optional<Transaction> transaction = transactionRepository.findById(id);
+        return transaction.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping(path = "/transactions/{id}")
+    public ResponseEntity<String> deleteTransaction(@PathVariable String id) {
+        transactionRepository.deleteById(id);
+        return ResponseEntity.ok(String.format("Document %s removed", id));
     }
 
 }
--- src/main/java/org/pauldenhez/AccountMe/model/Transaction.java
+++ src/main/java/org/pauldenhez/AccountMe/model/Transaction.java
@@ -1,5 +1,7 @@
 package org.pauldenhez.AccountMe.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
@@ -8,60 +10,26 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 import java.util.Date;
 
 @Document(indexName = "transactions")
+@Data
+@AllArgsConstructor
 public class Transaction {
 
     @Id
     private String id;
 
     @Field(type = FieldType.Date, name = "transaction_date")
-    private Date transactionDate;
+    private Date date;
 
     @Field(type = FieldType.Keyword)
-    private String label;
+    private String comment;
+
+    @Field(type = FieldType.Keyword)
+    private TransactionType type;
 
     @Field(type = FieldType.Double)
-    private Double credit;
+    private Double amount;
 
-    @Field(type = FieldType.Double)
-    private Double debit;
+    @Field(type = FieldType.Object)
+    private AdditionalInformation additionalInformation;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Date getTransactionDate() {
-        return transactionDate;
-    }
-
-    public void setTransactionDate(Date transactionDate) {
-        this.transactionDate = transactionDate;
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    public Double getCredit() {
-        return credit;
-    }
-
-    public void setCredit(Double credit) {
-        this.credit = credit;
-    }
-
-    public Double getDebit() {
-        return debit;
-    }
-
-    public void setDebit(Double debit) {
-        this.debit = debit;
-    }
 }
