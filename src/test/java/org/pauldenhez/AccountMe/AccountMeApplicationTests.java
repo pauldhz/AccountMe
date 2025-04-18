d62685a22f4265da40bd15f371c4256a527afed3
Maxime Rose <maxime.rose@hotmail.fr>
2025-04-18 11:16:01 +0200

    #4 - Fix tests

--- src/test/java/org/pauldenhez/AccountMe/AccountMeApplicationTests.java
+++ src/test/java/org/pauldenhez/AccountMe/AccountMeApplicationTests.java
@@ -3,11 +3,13 @@ package org.pauldenhez.accountme;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(classes = AccountMeApplication.class)
 class AccountMeApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(1 == 1);
 	}
-
 }
