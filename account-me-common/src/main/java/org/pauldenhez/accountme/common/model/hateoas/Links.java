386e06794214fa18076548e7492d073130e17de7
pauldhz <paul.denhez.ds@gmail.com>
2025-07-09 00:23:16 +0200

    feat(11-mettre-en-place-la-pagination): add page numbers

--- account-me-common/src/main/java/org/pauldenhez/accountme/common/model/hateoas/Links.java
+++ account-me-common/src/main/java/org/pauldenhez/accountme/common/model/hateoas/Links.java
@@ -1,3 +1,5 @@
 package org.pauldenhez.accountme.common.model.hateoas;
 
-public record Links(String next, String prev, String last, String first) {}
+import java.util.Map;
+
+public record Links(String next, String prev, String last, String first, Map<Integer, String> nNextLinks, int n) {}
--- account-me-common/src/main/java/org/pauldenhez/accountme/common/model/hateoas/LinksBuilder.java
+++ account-me-common/src/main/java/org/pauldenhez/accountme/common/model/hateoas/LinksBuilder.java
@@ -1,13 +1,27 @@
 package org.pauldenhez.accountme.common.model.hateoas;
 
-import io.micrometer.common.util.StringUtils;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public final class LinksBuilder {
     public static Links buildTransactionUriPagedLink(int size, int currentPageNumber, int totalPages, String baseUri) {
+        int n = 5;
         String next = "";
         String prev = "";
         String last = "";
         String first = "";
+
+        int rest = totalPages - currentPageNumber - n;
+        int firstNumber = rest < 0 ? currentPageNumber + rest + 1 : currentPageNumber;
+        int lastNumber = totalPages - currentPageNumber - n < 0 ? totalPages : currentPageNumber + n - 1;
+
+        int[] nNext = IntStream.rangeClosed(firstNumber, lastNumber).toArray();
+
+        Map<Integer, String> nLinks = IntStream.of(nNext).boxed().collect(Collectors.toMap(
+                i -> i,
+                i -> baseUri + "?size=" + size + "&page=" + i
+        ));
 
         if(currentPageNumber < totalPages) {
             next = baseUri + "?size=" + size + "&page=" + (currentPageNumber + 1);
@@ -17,7 +31,8 @@ public final class LinksBuilder {
             prev = baseUri + "?size=" + size + "&page=" + (currentPageNumber - 1);
             first = baseUri + "?size=" + size + "&page=1";
         }
-        return  new Links(next, prev, last, first);
+
+        return  new Links(next, prev, last, first, nLinks, n);
     }
 
 }
